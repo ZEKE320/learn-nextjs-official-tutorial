1660678660523a312e3f2484b0eaac8abcda0ea7
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-02-26 15:49:35 +0000

    🚧 Next.jsとPrismaを連携

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -1,3 +1,4 @@
+import prisma from '@/app/lib/prisma';
 import { sql } from '@vercel/postgres';
 import {
   CustomerField,
@@ -5,8 +6,8 @@ import {
   InvoiceForm,
   InvoicesTable,
   LatestInvoiceRaw,
-  User,
   Revenue,
+  User,
 } from './definitions';
 import { formatCurrency } from './utils';
 
@@ -21,11 +22,12 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    // const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data: Revenue[] = await prisma.revenue.findMany();
 
     // console.log('Data fetch completed after 3 seconds.');
 
-    return data.rows;
+    return data;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
